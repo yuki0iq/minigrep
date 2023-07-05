0651d3f723e4a1d749b3393060ffe503b119b461
Yuki <yuki.utk8g@gmail.com>
2023-07-06 00:51:36 +0300

    fix argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("find {} in file {}", query, file_path);
+    println!("find {} in file {}", config.query, config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("file should be available to read");
 
     println!("with text {}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path } 
+    }
 }
