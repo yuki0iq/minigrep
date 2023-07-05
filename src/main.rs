6fdb7501a089012f893e33aea413a4e9fa6c3372
Yuki <yuki.utk8g@gmail.com>
2023-07-06 01:15:07 +0300

    add simple error handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
-use std::env;
-use std::fs;
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing command line arguments: {err}");
+        process::exit(1);
+    });
 
     println!("find {} in file {}", config.query, config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("file should be available to read");
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("with text {}", contents);
+
+    Ok(())
 }
 
 struct Config {
     query: String,
-    file_path: String
+    file_path: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // TODO make more 'readable' error message
+            Err("Not enough arguments")
+        } else {
+            let query = args[1].clone();
+            let file_path = args[2].clone();
 
-        Config { query, file_path } 
+            Ok(Config { query, file_path })
+        }
     }
 }
